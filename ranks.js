fa13d0bfaa7c6ef0850f729ddbd254e0a6a4ac0b
eightycarry <calvin_lor@yahoo.com>
2020-11-03 15:33:05 -0600

    RankName

---
+++ ranks.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    $("form").submit(showRank);
+
+    function showRank(event){
+
+        //stop the flashing form
+        event.preventDefault();
+
+        //ask jQuery for first name
+        var firstName = $("#firstName").val();
+
+        //ask jQuery for last name
+        var lastName = $("#lastName").val();
+
+        //ask jQuery for selected radio boxes
+        var selectedBoxes = $("input[name=rank]:checked").val();
+
+        //output name and rank to table
+        $("#name1").text(firstName);
+        $("#name2").text(lastName);
+        $("#rankName").text(selectedBoxes);
+    };
+
+});
